c4818ea7bca49bc93ede069c47281bc48e92f7d5
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-10-08 16:06:12 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,14 @@ const buttonIcon = "🥠";
 const button = document.createElement("button");
 button.innerHTML = buttonIcon;
 app.append(button);
+
+const div = document.createElement("div");
+let counter: number = 0;
+div.innerHTML = `${counter} fortunes`;
+
+app.append(div);
+
+button.addEventListener("click", () => {
+  counter += 1;
+  div.innerHTML = `${counter} fortunes`;
+});
