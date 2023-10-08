b70cbf91c66d7afbbaed734297036538dcf4b1ac
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-10-08 15:43:35 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const buttonIcon = "🥠";
+const button = document.createElement("button");
+button.innerHTML = buttonIcon;
+app.append(button);
