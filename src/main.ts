ec1c5d5be7c502f464225d773db9946e5e6ec469
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-10-10 19:32:53 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -17,19 +17,23 @@ app.append(button);
 
 const div = document.createElement("div");
 let counter: number = 0;
-div.innerHTML = `${counter} fortunes`;
+div.innerHTML = `${counter} fortunes\n`;
 
 app.append(div);
+
+const buttonPurchasable = document.createElement("button");
+buttonPurchasable.innerHTML = "Tarot Card Deck [10 Fortunes]";
+buttonPurchasable.disabled = true;
+app.append(buttonPurchasable);
 
 button.addEventListener("click", () => {
   increment(1);
 });
 
-// setInterval(increment, 1000);
-// requestAnimationFrame(increment);
-
 let start: number, previousTimeStamp: number;
+let growth: number = 0;
 function add(timeStamp: number) {
+  // change function name
   if (start === undefined) {
     start = timeStamp;
   }
@@ -37,7 +41,14 @@ function add(timeStamp: number) {
   let count: number = 0;
 
   if (previousTimeStamp !== timeStamp && previousTimeStamp !== undefined) {
-    count = (timeStamp - previousTimeStamp) / 1000;
+    count = (timeStamp - previousTimeStamp) * 0.001 * growth;
+  }
+
+  if (counter >= 10) {
+    // remove magic number
+    buttonPurchasable.disabled = false;
+  } else {
+    buttonPurchasable.disabled = true;
   }
 
   increment(count);
@@ -45,9 +56,18 @@ function add(timeStamp: number) {
   requestAnimationFrame(add);
 }
 
-add(performance.now());
-
 function increment(count: number) {
   counter += count;
   div.innerHTML = `${counter} fortunes`;
+}
+
+add(performance.now());
+
+buttonPurchasable.addEventListener("click", () => {
+  counter -= 10;
+  increaseGrowth(1);
+});
+
+function increaseGrowth(grow: number) {
+  growth += grow;
 }
